2080ecdbe067bada9cbb16cda0bc1942c57200f1
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2025-06-11 01:01:03 +0800

    New Time period sprites + implementation

--- Assets/Scripts/New_HO_Scripts/HO_GameManager/TimePeriodButton.cs
+++ Assets/Scripts/New_HO_Scripts/HO_GameManager/TimePeriodButton.cs
@@ -9,12 +9,16 @@ public class TimePeriodButton : MonoBehaviour
     public bool isPreSelected = false;
 
     public bool wasSelected = false;
+    public Sprite pressedSprite;
+    public Button Button;
 
     private Image buttonImage;
+
 
     private void Awake()
     {
         buttonImage = GetComponent<Image>();
+        
     }
 
     public void SetHighlighted(bool selected)
@@ -25,6 +29,8 @@ public class TimePeriodButton : MonoBehaviour
     public void SetGreen()
     {
         buttonImage.color = Color.green;
+        Image changeImg = Button.GetComponent<Image>();
+        changeImg.sprite = pressedSprite;
     }
 
     public void SetSelected(bool selected)
